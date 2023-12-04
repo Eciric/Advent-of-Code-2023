d348b54ae55bd0ade107a8bea229932f2b72da31
Przemysław Kaczmarski <nyviel111@gmail.com>
2023-12-04 12:58:59 +0100

    Shortened the code to the maximum

--- Day 4/index.js
+++ Day 4/index.js
@@ -1,73 +1,36 @@
 const fs = require("fs");
 
 fs.readFile("data.txt", "utf-8", (_, data) => {
-	solve(data.trim().split("\r\n"));
-});
+	data = data.trim().split("\r\n");
+	let ans = 0;
+	const cardsOfType = data.map((_, i) => 1);
 
-const solve = (data) => {
-	let parsedCards = parseCards(data);
-	// countPoints(parsedCards);
-	duplicateCards(parsedCards);
-};
-
-const parseCards = (cards) => {
-	const parsedCards = [];
-	for (const card of cards) {
-		let row = card.split(":")[1];
-		let [winningNumbers, scratchNumbers] = row.trim().split("|");
-
-		parsedCards.push({
-			winningNumbers: new Set(formatCardNumbers(winningNumbers)),
-			scratchNumbers: new Set(formatCardNumbers(scratchNumbers)),
-		});
-	}
-	return parsedCards;
-};
-
-const countPoints = (cards) => {
-	let totalPoints = 0;
-	for (const card of cards) {
-		let cardPoints = 0;
-		for (const winningNumber of card.winningNumbers) {
-			if (card.scratchNumbers.has(winningNumber)) {
-				cardPoints ? (cardPoints *= 2) : (cardPoints = 1);
-			}
-		}
-		totalPoints += cardPoints;
-	}
-	console.log(totalPoints);
-};
-
-const formatCardNumbers = (numbers) => {
-	return numbers
-		.trim()
-		.split(" ")
-		.filter((num) => num != "")
-		.map(Number);
-};
-
-const duplicateCards = (cards) => {
-	const cardsOfType = {};
-	for (const [i, card] of cards.entries()) {
-		cardsOfType[i] = 1;
-	}
-	for (let i = 0; i < cards.length; i++) {
+	for (const [i, line] of data.entries()) {
 		cardsOfType[i] += 1;
+		let [winningNumbers, scratchNumbers] = line
+			.split(":")[1]
+			.trim()
+			.split("|")
+			.map((el) =>
+				el
+					.trim()
+					.split(" ")
+					.filter((num) => num != "")
+					.map(Number)
+			);
 
-		let cardsToDuplicate = 0;
-		for (const winningNumber of cards[i].winningNumbers) {
-			if (cards[i].scratchNumbers.has(winningNumber)) {
-				cardsToDuplicate++;
+		const cross = winningNumbers.filter((number) =>
+			scratchNumbers.includes(number)
+		);
+		if (cross.length) {
+			ans += 2 ** (cross.length - 1);
+			for (let j = 0; j < cross.length; j++) {
+				cardsOfType[i + 1 + j] += cardsOfType[i];
 			}
-		}
-
-		for (let j = 0; j < cardsToDuplicate; j++) {
-			cardsOfType[i + 1 + j] += cardsOfType[i];
 		}
 	}
 	console.log(
-		Object.values(cardsOfType).reduce((prev, cur) => {
-			return prev + cur;
-		}, 0) / 2
+		ans,
+		Object.values(cardsOfType).reduce((prev, cur) => prev + cur, 0) / 2
 	);
-};
+});
