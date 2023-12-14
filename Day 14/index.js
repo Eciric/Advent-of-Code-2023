92fb9995f96714bae5495c2fe450794bfd88feb7
Przemysław Kaczmarski <nyviel111@gmail.com>
2023-12-14 11:01:17 +0100

    Day 14 part 2

--- Day 14/index.js
+++ Day 14/index.js
@@ -5,10 +5,53 @@ fs.readFile("data.txt", "utf-8", (_, data) => {
 });
 
 const solve = (data) => {
+	partOne(data);
+	partTwo(data);
+};
+
+const partOne = (data) => {
+	tiltNorth(data);
+	calculateLoad(data);
+};
+
+const partTwo = (data) => {
+	// No need to do 1B cycles it aligns anyway
+	for (let i = 0; i < 1000; i++) {
+		cycle(data);
+	}
+	calculateLoad(data);
+	fs.writeFile(
+		"output.txt",
+		JSON.stringify(data.map((row) => row.join(""))),
+		() => {}
+	);
+};
+
+const cycle = (data) => {
+	tiltNorth(data);
+	tiltWest(data);
+	tiltSouth(data);
+	tiltEast(data);
+};
+
+const calculateLoad = (data) => {
+	data = data.map((row) => row.join(""));
+	let load = 0;
+	for (const [i, row] of data.entries()) {
+		for (const cell of row) {
+			if (cell == "O") {
+				load += data.length - i;
+			}
+		}
+	}
+	console.log(load);
+};
+
+const tiltNorth = (data) => {
 	data = data.reverse();
-	let totalMovements = 1;
-	while (totalMovements) {
-		totalMovements = 0;
+	let titls = 1;
+	while (titls) {
+		titls = 0;
 		for (let i = 0; i < data.length - 1; i++) {
 			for (let j = 0; j < data[i].length; j++) {
 				const cell = data[i][j];
@@ -16,19 +59,64 @@ const solve = (data) => {
 				if (cell === "O" && cellBelow === ".") {
 					data[i][j] = ".";
 					data[i + 1][j] = "O";
-					totalMovements++;
+					titls++;
 				}
 			}
 		}
 	}
-	data = data.reverse().map((row) => row.join(""));
-	let weight = 0;
-	for (const [i, row] of data.entries()) {
-		for (const cell of row) {
-			if (cell == "O") {
-				weight += data.length - i;
+	data = data.reverse();
+};
+
+const tiltSouth = (data) => {
+	let titls = 1;
+	while (titls) {
+		titls = 0;
+		for (let i = 0; i < data.length - 1; i++) {
+			for (let j = 0; j < data[i].length; j++) {
+				const cell = data[i][j];
+				const cellBelow = data[i + 1][j];
+				if (cell === "O" && cellBelow === ".") {
+					data[i][j] = ".";
+					data[i + 1][j] = "O";
+					titls++;
+				}
 			}
 		}
 	}
-	console.log(weight);
+};
+
+const tiltEast = (data) => {
+	let titls = 1;
+	while (titls) {
+		titls = 0;
+		for (let i = 0; i < data.length; i++) {
+			for (let j = 0; j < data[i].length - 1; j++) {
+				const cell = data[i][j];
+				const cellRight = data[i][j + 1];
+				if (cell === "O" && cellRight === ".") {
+					data[i][j] = ".";
+					data[i][j + 1] = "O";
+					titls++;
+				}
+			}
+		}
+	}
+};
+
+const tiltWest = (data) => {
+	let titls = 1;
+	while (titls) {
+		titls = 0;
+		for (let i = 0; i < data.length; i++) {
+			for (let j = data[i].length - 1; j > 0; j--) {
+				const cell = data[i][j];
+				const cellLeft = data[i][j - 1];
+				if (cell === "O" && cellLeft === ".") {
+					data[i][j] = ".";
+					data[i][j - 1] = "O";
+					titls++;
+				}
+			}
+		}
+	}
 };
